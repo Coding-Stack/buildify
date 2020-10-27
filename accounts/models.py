0182f2de65ce0b940f71837105fec30ccd663baf
abhishek0601 <abhishek.nithya@gmail.com>
2020-10-27 23:02:38 +0530

    Worker,client and admin tables have been added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Worker(models.Model):
+    wages = models.FloatField()
+    prev_record = models.IntegerField()
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    work_done = [
+        ('Electrician','Electrician'),
+        ('Carpenter','Carpenter'),
+        ('Plumber','Plumber'),
+        ('Painter','Painter'),
+        ('Cons_worker','Cons_worker'),
+        ('None','None'),
+    ]
+
+class Client(models.Model):
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
+
+class Admin(models.Model):
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
